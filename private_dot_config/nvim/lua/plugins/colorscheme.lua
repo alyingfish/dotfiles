164d42ef3dfef056a77c2f410f93413aac85bfff
lyingfish's ROG <unlyingfish@gmail.com>
2026-06-25 17:27:38 +0800

    update 6/25

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,22 @@
 return {
   {
+    "catppuccin/nvim",
+    opts = {
+      transparent_background = true,
+      float = {
+        transparent = true,
+        solid = true,
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-latte",
+      -- check: https://github.com/LazyVim/LazyVim/discussions/2054
+      -- Catppuccin has changed its name to catppuccin-nvim (the name that you pass to vim.cmd.colorscheme)
+      -- because Neovim 0.12 integrated another catppuccin colorscheme into its builtin ones (they are not the same).
+      -- So you have to to change colorscheme = "catppuccin" to colorscheme = "catppuccin-nvim".
+      colorscheme = "catppuccin-nvim",
     },
   },
 }
